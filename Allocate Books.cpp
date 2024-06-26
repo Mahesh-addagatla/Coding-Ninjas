0ae7b541331d80958440d48b2aaa561a30217e59
MAHESH ADDAGATLA <158994925+Mahesh-addagatla@users.noreply.github.com>
2024-06-26 14:55:09 +0530

    Create Allocate Books.cpp

---
+++ Allocate Books.cpp
@@ -0,0 +1,66 @@
+int findPages(vector<int>& arr, int n, int m) {
+    // Write your code here.
+     if(m>n)
+
+    {
+
+        return -1;
+
+    }
+
+    int start = 0,end=0,ans,mid;
+
+    for(int i=0;i<n;i++)
+
+    {
+
+        start = max(start,arr[i]);
+
+        end+=arr[i];
+
+    }
+
+    while(start<=end)
+
+    {
+
+        mid = start+(end-start)/2;
+
+        int pages = 0,count = 1;
+
+        for(int i=0;i<n;i++)
+
+        {
+
+            pages+=arr[i];
+
+            if(pages>mid)
+
+            {
+
+                count++;
+
+                pages= arr[i];
+
+            }
+
+        }
+
+        if(count<=m)
+
+        {
+
+            ans = mid;
+
+            end=mid-1;
+
+        }
+
+        else start=mid+1;
+
+        
+
+    }
+
+     return ans;
+}
