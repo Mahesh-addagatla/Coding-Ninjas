9bc367b033ed9161812162d4f3166d7e27748a25
MAHESH ADDAGATLA <158994925+Mahesh-addagatla@users.noreply.github.com>
2024-07-13 23:08:24 +0530

    Create Odd and even positioned linked list nodes.java

---
+++ Odd and even positioned linked list nodes.java
@@ -0,0 +1,35 @@
+import java.util.* ;
+import java.io.*; 
+/****************************************************************
+
+    Following is the class structure of the Node class:
+
+	class Node {
+	    public int value;
+	    public Node next;
+
+	    Node(int value) {
+	        this.value = value;
+	        this.next = null;
+	    }
+	}
+
+*****************************************************************/
+
+public class Solution {
+	public static Node oddEvenLinkedList(Node head) {
+		// Write your code here.
+		if(head==null || head.next==null)return head;
+		Node even=head.next;
+		Node odd=head;
+		Node evenhead=even;
+		while(even!=null && even.next!=null){
+			odd.next=even.next;
+			odd=odd.next;
+			even.next=odd.next;
+			even=even.next;
+		}
+		odd.next=evenhead;
+		return head;
+	}
+}
