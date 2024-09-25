d7c451a6efcc7d8a79c9c1c486fc34d12976aa93
MAHESH ADDAGATLA <158994925+Mahesh-addagatla@users.noreply.github.com>
2024-09-25 20:52:54 +0530

    Create Detect the Cycle.java

---
+++ Detect the Cycle.java
@@ -0,0 +1,29 @@
+/************************************************************************
+
+    Following is the representation of the Singly Linked List node:
+
+    class Node<T> {
+        public T data;
+        public Node<T> next;
+
+        public Node(T data) {
+            this.data = data;
+            this.next = null;
+        }
+    }
+
+************************************************************************/
+
+public class Solution {
+    public static boolean detectCycle(Node<Integer> head) {
+        // Write your code here.
+        Node slow=head;
+        Node fast=head;
+        while(fast!=null&& fast.next!=null ){
+            slow=slow.next;
+            fast=fast.next.next;
+            if(slow==fast)return true;
+        }
+        return false;
+    }
+}
